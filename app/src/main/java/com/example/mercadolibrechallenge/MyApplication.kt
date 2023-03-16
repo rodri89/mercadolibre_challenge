d3bd7777db1d14975ab1a1e26e9466efc0071cc2
rodri89 <banegasrodrigo89@gmail.com>
2023-03-16 20:06:25 -0300

    Setup project.

---
+++ app/src/main/java/com/example/mercadolibrechallenge/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.example.mercadolibrechallenge
+
+import android.app.Application
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/example/mercadolibrechallenge/ui/activity/SearchActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mercadolibrechallenge.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mercadolibrechallenge.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_activity)
+    }
+
+}
